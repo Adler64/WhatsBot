66f73f3d6e2c50a0288413d7b22fa889a40c74f1
João Adler <72223931+Adler64@users.noreply.github.com>
2024-01-10 17:36:09 +0000

    Inserção de nova biblioteca

--- app_bot.py
+++ app_bot.py
@@ -10,7 +10,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-
+import openpyxl
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
